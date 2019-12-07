99f5ca62977c09e708013d1ce51fb6019d34aba9
Shin'ya Ueoka <ueokande@i-beam.org>
2019-12-07 14:22:43 +0900

    Remove dev and test scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CheatSheet from './components/CheatSheet';
-import * as serviceWorker from './serviceWorker';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
@@ -26,8 +25,3 @@ ReactDOM.render(
     <CheatSheet />
   </React.Fragment>
   , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
