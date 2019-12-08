b16af5b600a394eac5726770acc834121c16f236
Shin'ya Ueoka <ueokande@i-beam.org>
2019-12-08 14:59:21 +0900

    Update styles

--- src/components/CardTitle/index.tsx
+++ src/components/CardTitle/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Title = styled.h2`
-  font-size: 1.2rem;
+  font-size: 1.5rem;
   line-height: 1.5rem;
   margin: 0.5rem 0;
 `;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,19 +7,26 @@ interface Props {
   link: string;
 }
 
-const Container = styled(Card)``;
+const Container = styled.div`
+  background-color: #333;
+  color: white;
+  max-width: 35rem;
+  padding: 1rem;
+  margin: 0.5rem;
+  box-shadow: 0 0 0.4rem gray;
+`;
 
 const Title = styled.h1``;
 
 const Link = styled.a`
   &:link,
   &:visited {
-    color: #333;
+    color: unset;
     text-decoration: none;
   }
   &:hover,
   &:active {
-    color: #333;
+    color: unset;
     text-decoration: underline;
   }
 `;
