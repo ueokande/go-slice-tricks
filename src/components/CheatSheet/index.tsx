c3ab195574050639df8285904f96c70b4ab8cbc5
Shin'ya UEOKA <ueokande@i-beam.org>
2019-12-10 03:11:29 +0000

    Fix reference link

--- src/components/CheatSheet/index.tsx
+++ src/components/CheatSheet/index.tsx
@@ -46,7 +46,7 @@ const CheatSheet: React.FC = () => {
       <Header
         title="Go Slice Tricks Cheat Sheet"
         author="@ueokande"
-        link="https://ueokande.github.io/go-slice-tricks/"
+        link="https://github.com/golang/go/wiki/SliceTricks"
       />
       <Card>
         <CardTitle>AppendVector</CardTitle>
